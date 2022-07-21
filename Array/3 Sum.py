9ab98ff1102b3f26c910c736554f5af5a1efc166
Robert Ndungu <robert.ndungu@hotmail.com>
2022-07-21 19:57:33 +0300

    - comments

--- Array/3 Sum.py
+++ Array/3 Sum.py
@@ -6,16 +6,23 @@ Notice that the solution set must not contain duplicate triplets.
 """
 class Solution:
     """
-    1. sort the array
+    1. sort the array.
+    2. for similar numbers following each other, skip all the duplicates. this is to avoid duplicate triplets when we do a two sum.
     2. for every element in the array, do a two sum of the other elements in the array
         - two sum is done by two pointers, left and right.
         - if sum is larger than target sum, move right pointer to the left of the current element, and vice versa.
-    - if sum is equal to target sum, add the triplet to the result list.
+        - if sum is equal to target sum, add the triplet to the result list.
+        - move the left pointer forward, and the right pointer backward.
+        - if the number on the left is same to the previous number, skip it.
+        - if the number on the right is same to the number ahead of it, skip it.(these two steps are to avoid duplicate triplets)
     """
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         return_array = []
         nums.sort()
         for i in range(len(nums)):
+            # if current element is equal to the previous element, skip it. 
+            # this is because when we do a two sum, we will get duplicate triplets.
+            # this is because we are doing a two sum of the same element, and the array is sorted.
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             left = i + 1
@@ -36,3 +43,8 @@ class Solution:
                         right -= 1
 
         return return_array
+"""
+Complexity.
+- Time is O(n^2) - we are looping through all elements with neseted loops + o(nlogn) for sorting the array => O(n^2)
+- Space is O(n) -  for the return array and also for sorting the array(depends on the sorting algorithm) => O(n)
+"""
--- Array/Two Sum.py
+++ Array/Two Sum.py
@@ -3,7 +3,7 @@ Given an array of integers nums and an integer target, return indices of the two
 '''
 def twoSums(nums, integer):
     """
-    1. create a dict.
+    1. create a dict. key will be the integer difference between target and nums[i], value is index of nums[i].
     2. loop thro' the array, check if current element is in the dict, if it is, return current index and value from the dict.
         - else, create a dict entry with difference as key and index as the value.
     """
@@ -18,4 +18,29 @@ def twoSums(nums, integer):
 Complexity.
 - Time is O(n) - we are looping through all elements
 - Space is O(n) - for the dict
+"""
+def twoSums(nums, integer):
+    """
+    solution with two pointers
+    """
+    nums.sort()
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        sum = nums[left] + nums[right]
+        if sum < integer:
+            left += 1
+        elif sum > integer:
+            right -= 1
+        else:
+            return (left, right)
+        while left < right and nums[left] == nums[left-1]:
+            left += 1
+        while left < right and nums[right] == nums[right+1]:
+            right -= 1
+    return None
+"""
+Complexity.
+- Time is O(nlog(n)) - we are sorting the array
+- Space is O(1)
 """
